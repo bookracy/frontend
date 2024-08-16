a7e1c44d476d450bc9569d60c051db77fa9bf09e
Jorrin <jorrinkievit@hotmail.com>
2024-08-17 01:22:22 +0200

    revert theme-toggle redundant changes

--- src/components/layout/theme-toggle.tsx
+++ src/components/layout/theme-toggle.tsx
@@ -13,15 +13,11 @@ export function ThemeToggle(props: ThemeToggleProps) {
   const theme = useSettingsStore((state) => state.theme);
   const setTheme = useSettingsStore((state) => state.setTheme);
 
-  const handleThemeChange = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
-
   return (
     <TooltipProvider disableHoverableContent>
       <Tooltip delayDuration={100}>
         <TooltipTrigger asChild>
-          <Button className={cn("h-8 w-8 rounded-full bg-background", props.className)} variant="outline" size="icon" onClick={handleThemeChange}>
+          <Button className={cn("h-8 w-8 rounded-full bg-background", props.className)} variant="outline" size="icon" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-transform duration-500 ease-in-out dark:rotate-0 dark:scale-100" />
             <Moon className="scale-1000 absolute h-[1.2rem] w-[1.2rem] rotate-0 transition-transform duration-500 ease-in-out dark:-rotate-90 dark:scale-0" />
             <span className="sr-only">Switch Theme</span>

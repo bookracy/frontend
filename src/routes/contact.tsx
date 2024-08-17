375c7677c470379eedfc6807e5ffc57594b8a4ae
Cooper <Cooperransom08@gmail.com>
2024-08-17 01:27:00 +0000

    Small style changes and start the contact page

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,5 +1,31 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Switch } from "@/components/ui/switch";
+import { LANGUAGES } from "@/constants";
+import { useSettingsStore } from "@/stores/settingsStore";
 import { createFileRoute } from "@tanstack/react-router";
+import { Lock } from "lucide-react";
 
 export const Route = createFileRoute("/contact")({
-  component: () => <div>Hello /contact!</div>,
+  component: Contact,
 });
+
+function Contact() {
+  const { booksPerSearch, setBooksPerSearch, language, setLanguage, backendURL, setBackendURL } = useSettingsStore();
+  return (
+    <div className="flex h-full justify-center">
+      <div className="flex flex-col gap-5 w-full">
+        <Card>
+          <CardHeader>
+            <CardTitle>Contact Us</CardTitle>
+            <CardDescription>
+              idk some yap
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      </div>
+    </div>
+  );
+}

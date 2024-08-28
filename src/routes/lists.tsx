d82463d6d896254ba5d48fa91c317e9c726c2e25
Cooper <Cooperransom08@gmail.com>
2024-08-28 02:53:59 +0000

    improve list page's context

--- src/routes/lists.tsx
+++ src/routes/lists.tsx
@@ -1,6 +1,7 @@
 import { BookList } from "@/components/books/book-list";
 import { useBookmarksStore } from "@/stores/bookmarks";
 import { createFileRoute } from "@tanstack/react-router";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 export const Route = createFileRoute("/lists")({
   component: Lists,
@@ -12,7 +13,24 @@ export function Lists() {
   return (
     <div className="flex flex-1 justify-center">
       <div className="flex w-full flex-col gap-4">
-        {bookmarks.length > 0 ? <h1 className="text-2xl font-bold">Your Bookmarks</h1> : null}
+        {bookmarks.length > 0 ? (
+          <h1 className="text-2xl font-bold">Your Bookmarks</h1>
+        ):(
+          <div>
+            <TooltipProvider delayDuration={0}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <h1 className="text-2xl font-bold">No Bookmarks</h1>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>
+                    Add some books here by clicking the bookmark icon on a book card.
+                  </p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
+        )}
 
         <BookList books={bookmarks} />
       </div>
